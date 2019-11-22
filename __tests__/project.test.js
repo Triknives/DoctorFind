7a9d34e703894e34fa9ab6267fbfd0c47a822e51
Triknives <50305423+Triknives@users.noreply.github.com>
2019-11-22 09:22:52 -0800

    set up initial testing structure for jest.

---
+++ __tests__/project.test.js
@@ -0,0 +1,12 @@
+import { method  } from './../src/doctorFind.js';
+
+describe(, () => {
+  test(, () => {
+    let  = new ();
+    expect().toEqual();
+  });
+  test( () => {
+    let  = new ();
+    expect().toEqual();
+  });
+});
