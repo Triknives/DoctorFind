b6f5ce97fddabeb7672a1927b680d7244091edd7
Triknives <50305423+Triknives@users.noreply.github.com>
2019-11-22 12:05:50 -0800

    update request url and constructor key to match spec

--- src/doctorFind.js
+++ src/doctorFind.js
@@ -1,12 +1,11 @@
 export class DoctorFind {
   constructor (location, query) {
-      this.location = location;
       this.query = query;
   }
 
   async getDoctorInfo() {
     try {
-      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${this.query}&location=${this.location}&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${this.query}&location=or-portland&skip=0&limit=10&user_key=${process.env.API_KEY}`);
       let jsonifiedResponse = await response.json();
       return jsonifiedResponse;
     } catch(error) {
