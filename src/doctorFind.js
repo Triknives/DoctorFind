5f050954a9702b5756740fa7ba794aba58f5e651
Triknives <50305423+Triknives@users.noreply.github.com>
2019-11-22 14:12:25 -0800

    now provide app with ability to pull first name of doctor, will work to add further doctor info such as last name.

--- src/doctorFind.js
+++ src/doctorFind.js
@@ -1,8 +1,9 @@
 export class DoctorFind {
-  async getDoctorInfo(query) {
+  async getDoctorInfo(nameInput) {
     try {
       let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${query}&location=or-portland&skip=0&limit=10&user_key=${process.env.API_KEY}`);
       let jsonifiedResponse = await response.json();
+      console.log(jsonifiedResponse);
       return jsonifiedResponse;
     } catch(error) {
       console.error("There was an error handling your request: " + error.message);
--- src/main.js
+++ src/main.js
@@ -9,15 +9,17 @@ $(document).ready(function() {
   $('#searchSubmit').submit(function(event){
     event.preventDefault();
 
+
+    const nameInput =$("#queryInput").val();
+
     (async () => {
       let doctorFindInfo = new DoctorFind();
-      const response = await doctorFindInfo.getDoctorInfo();
+      const response = await doctorFindInfo.getDoctorInfo(query);
       getDoctorElement(response);
     })();
 
     const getDoctorElement = (response) => {
       $('#doctorSearch').text(response.data[0].profile.first_name);
-      console.log(response);
     };
   });
 });
