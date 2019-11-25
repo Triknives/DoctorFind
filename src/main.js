d45e6a8751a13dd82d5a897454562fd6e4cc57e5
Triknives <50305423+Triknives@users.noreply.github.com>
2019-11-24 18:29:40 -0800

    add another field for results found within index 1 during query.

--- src/main.js
+++ src/main.js
@@ -9,7 +9,6 @@ $(document).ready(function() {
   $('#searchSubmit').submit(function(event){
     event.preventDefault();
 
-
     const name =$("#nameInput").val();
 
     (async () => {
@@ -19,16 +18,27 @@ $(document).ready(function() {
     })();
 
     const getDoctorElement = (response) => {
+      response.data[0];
       $('#nameSearch').text(" " + response.data[0].profile.first_name +" "+ response.data[0].profile.last_name);
       $('#contactNumber').text(" " +response.data[0].practices[0].phones[0].number);
       $('#address').text(" " +response.data[0].practices[0].visit_address.street);
       $('#acceptingClients').text(" " +response.data[0].practices[0].accepts_new_patients);
-
-      //Below is branch logic to provide user within information on whether a website is available for the doctor.
       if (response.data[0].practices[0].website === undefined){
         return $('#website').text(" " + "Sorry, they don't appear to have a website!");
       }else
-       $('#website').text(" " + response.data[0].practices[0].website);
+      $('#website2').text(" " + response.data[1].practices[0].website);
+
+
+      
+      //Below will display doctor number 2 within search array.
+      $('#nameSearch2').text(" " + response.data[1].profile.first_name +" "+ response.data[1].profile.last_name);
+      $('#contactNumber2').text(" " +response.data[1].practices[0].phones[0].number);
+      $('#address2').text(" " +response.data[1].practices[0].visit_address.street);
+      $('#acceptingClients2').text(" " +response.data[1].practices[0].accepts_new_patients);
+      if (response.data[1].practices[0].website === undefined){
+        return $('#website2').text(" " + "Sorry, they don't appear to have a website!");
+      }else
+      $('#website2').text(" " + response.data[1].practices[0].website);
     };
   });
 });
