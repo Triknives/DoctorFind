06a9babdf91088fa9ed7d16fa4382aca0ad07b52
Triknives <50305423+Triknives@users.noreply.github.com>
2019-11-29 10:45:45 -0800

    update branch logic to provide accurate output of doctor website and if they are accepting clients at this time.

--- src/main.js
+++ src/main.js
@@ -17,15 +17,22 @@ $(document).ready(function() {
     })();
     const getDoctorElement = (response) => {
       for (let i = 0; i < response.data.length; i++) {
-        $('#searchResults').append('<li>' + response.data[i].profile.first_name + " "+ response.data[i].profile.last_name + '</li>' );
+        $('#searchResults').append('<li>'+ '<strong>' + response.data[i].profile.first_name + " "+ response.data[i].profile.last_name + '</strong>' +'</li>' );
         $('#searchResults').append('<li>' + response.data[i].practices[0].phones[0].number) + '</li>';
         $('#searchResults').append('<li>' + response.data[i].practices[0].visit_address.street + '</li>');
-        $('#searchResults').append('<li>' + "Are they accepting clients?" + " " + response.data[0].practices[0].accepts_new_patients) + '</li>';
-        // if (response.data[0].practices[0].website === undefined){
-        //   return $('#searchResults').append("<li>" + "Sorry, they don't appear to have a website!" + "</li>");
-        // }else
-        // $('#searchResults').append("<li>" + response.data[0].practices[0].website + "</li>");
+
+        if (response.data[i].practices[0].website === undefined){
+          $('#searchResults').append("<li>" + "Sorry, they don't appear to have a website!" + "</li>");
+        }else{
+          $('#searchResults').append("<li>" + response.data[i].practices[0].website + "</li>");
+        }
+
+        if (response.data[i].practices[0].accepts_new_patients === false){
+          $('#searchResults').append("<li>" + "Not currently taking new clients." + "</li>");
+        }else{
+          $('#searchResults').append("<li>" + "Currently taking new clients." + "</li>");
+        }
       }
-    };
-  });
+    }
+});
 });
