0a0247193064166f68887a6a52ab53e0e2cceecd
Triknives <50305423+Triknives@users.noreply.github.com>
2019-12-01 14:24:51 -0800

    added 2nd form for issue query. Only operates when initial form is used. Currently debugging

--- src/main.js
+++ src/main.js
@@ -17,35 +17,51 @@ $(document).ready(function() {
     })();
 
     $("#issueButton").click(function(){
-      event.preventDefault();
       const issue =$("#issueInput").val();
-      $('#searchSubmit').hide();
       $('#searchResults').show();
-
+      console.log(issue);
 
       (async () => {
         let issueFindInfo = new DoctorFind();
         const issueInput = await issueFindInfo.getIssueInfo(issue);
-        getDoctorElement(issueInput);
+        getIssueElement(issueInput);
       })();
     });
 
-      const getDoctorElement = (response) => {
-        for (let i = 0; i < response.data.length; i++) {
-          $('#searchResults').append('<li>'+ '<strong>' + response.data[i].profile.first_name + " "+ response.data[i].profile.last_name + '</strong>' +'</li>' );
-          $('#searchResults').append('<li>' + response.data[i].practices[0].phones[0].number) + '</li>';
-          $('#searchResults').append('<li>' + response.data[i].practices[0].visit_address.street + '</li>');
-          if (response.data[i].practices[0].website === undefined){
-            $('#searchResults').append("<li>" + "Sorry, they don't appear to have a website!" + "</li>");
-          }else{
-            $('#searchResults').append("<li>" + response.data[i].practices[0].website + "</li>");
-          }
-          if (response.data[i].practices[0].accepts_new_patients === false){
-            $('#searchResults').append("<li>" + "Not currently taking new clients." + "</li>");
-          }else{
-            $('#searchResults').append("<li>" + "Currently taking new clients." + "</li>");
-          }
+    const getDoctorElement = (response) => {
+      for (let i = 0; i < response.data.length; i++) {
+        $('#searchResults').append('<li>'+ '<strong>' + response.data[i].profile.first_name + " "+ response.data[i].profile.last_name + '</strong>' +'</li>' );
+        $('#searchResults').append('<li>' + response.data[i].practices[0].phones[0].number) + '</li>';
+        $('#searchResults').append('<li>' + response.data[i].practices[0].visit_address.street + '</li>');
+        if (response.data[i].practices[0].website === undefined){
+          $('#searchResults').append("<li>" + "Sorry, they don't appear to have a website!" + "</li>");
+        }else{
+          $('#searchResults').append("<li>" + response.data[i].practices[0].website + "</li>");
         }
-      };
-    });
+        if (response.data[i].practices[0].accepts_new_patients === false){
+          $('#searchResults').append("<li>" + "Not currently taking new clients." + "</li>");
+        }else{
+          $('#searchResults').append("<li>" + "Currently taking new clients." + "</li>");
+        }
+      }
+    };
+    const getIssueElement = (issueInput) => {
+      for (let i = 0; i < issueInput.data.length; i++) {
+        $('#searchResults').append('<li>'+ '<strong>' + issueInput.data[i].profile.first_name + " "+ issueInput.data[i].profile.last_name + '</strong>' +'</li>' );
+        console.log(issueInput);
+        $('#searchResults').append('<li>' + issueInput.data[i].practices[0].phones[0].number) + '</li>';
+        $('#searchResults').append('<li>' + issueInput.data[i].practices[0].visit_address.street + '</li>');
+        if (issueInput.data[i].practices[0].website === undefined){
+          $('#searchResults').append("<li>" + "Sorry, they don't appear to have a website!" + "</li>");
+        }else{
+          $('#searchResults').append("<li>" + issueInput.data[i].practices[0].website + "</li>");
+        }
+        if (issueInput.data[i].practices[0].accepts_new_patients === false){
+          $('#searchResults').append("<li>" + "Not currently taking new clients." + "</li>");
+        }else{
+          $('#searchResults').append("<li>" + "Currently taking new clients." + "</li>");
+        }
+      }
+    };
   });
+});
