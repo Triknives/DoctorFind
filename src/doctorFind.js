f4d5c797774de4df39ed5e8a42836a814e28d4ea
Triknives <50305423+Triknives@users.noreply.github.com>
2019-12-08 13:59:33 -0800

    update API error handling, and output to user.

--- src/doctorFind.js
+++ src/doctorFind.js
@@ -1,22 +1,29 @@
+import $ from 'jquery';
 export class DoctorFind {
   async getDoctorInfo(name) {
     try {
       let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&skip=0&limit=5&user_key=${process.env.API_KEY}`);
-      let jsonifiedResponse = await response.json();
-      return jsonifiedResponse;
-    } catch(error) {
-      $('#searchResults').text("Sorry, there was an issue with your request." + error.message);
-      console.error("There was an error handling your request: " + error.message);
+        let jsonifiedResponse = await response.json();
+        if(response.ok === false){
+          $('#searchResults').text("Sorry, there was an issue with your request.");
+        }
+        return jsonifiedResponse;
+      } catch(error) {
+        console.error(error.message);
+        $('#searchResults').text("Sorry, there was an issue with your request." + error.message);
+      }
     }
-  }
-  async getIssueInfo(issue) {
-    try {
-      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=or-portland&skip=0&limit=5&user_key=${process.env.API_KEY}`);
-      let jsonifiedResponse = await response.json();
-      return jsonifiedResponse;
-    } catch(error) {
-      $('#searchResults').text("Sorry, there was an issue with your request." + error.message);
-      console.error("There was an error handling your request: " + error.message);
-    }
-  }
-}
+    async getIssueInfo(issue) {
+      try {
+        let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=or-portland&skip=0&limit=5&user_key=${process.env.API_KEY}`);
+          let jsonifiedResponse = await response.json();
+          if(response.ok === false){
+            $('#searchResults').text("Sorry, there was an issue with your request.");
+          }
+          return jsonifiedResponse;
+          } catch(error) {
+            console.error(error.message);
+              $('#searchResults').text("Sorry, there was an issue with your request." + error.message);
+          }
+        }
+      }
